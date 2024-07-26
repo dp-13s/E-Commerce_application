79554254c04d7efa781ffa3baaf634b377a6a275
Sourav2507 <132735088+Sourav2507@users.noreply.github.com>
2024-07-27 00:21:43 +0530

    Rectified OnBoardingActivity

--- app/src/main/java/com/example/myapplication/OnBoardingActivity.java
+++ app/src/main/java/com/example/myapplication/OnBoardingActivity.java
@@ -18,13 +18,17 @@ import androidx.viewpager.widget.ViewPager;
 
 public class OnBoardingActivity extends AppCompatActivity {
 
+
     ViewPager viewPager;
     LinearLayout dotsLayout;
-    SliderAdapter sliderAdapter;
 
     TextView[] dots;
-    Animation animation;
+
     Button btn;
+    SliderAdapter sliderAdapter;
+
+    Animation animation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,20 +39,22 @@ public class OnBoardingActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_on_boarding);
 
-        //hide toolbar
-        getSupportActionBar().hide();
 
+        // Initialize the views after setting the content view
         viewPager = findViewById(R.id.slider);
         dotsLayout = findViewById(R.id.dots);
-        btn = findViewById(R.id.get_started_btn);
+
         addDots(0);
+
+        btn = findViewById(R.id.get_started_btn);
+
         viewPager.addOnPageChangeListener(changeListener);
 
-
+        // Set up the adapter
         sliderAdapter = new SliderAdapter(this);
         viewPager.setAdapter(sliderAdapter);
-    }
 
+    }
     private void addDots(int position){
 
         dots = new TextView[3];
@@ -64,15 +70,17 @@ public class OnBoardingActivity extends AppCompatActivity {
             dots[position].setTextColor(getResources().getColor(R.color.pink));
         }
     }
+
     ViewPager.OnPageChangeListener changeListener = new ViewPager.OnPageChangeListener() {
         @Override
         public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
         }
-
         @Override
         public void onPageSelected(int position) {
+
             addDots(position);
+
             if(position == 0){
                 btn.setVisibility(TextView.INVISIBLE);
             } else if (position == 1) {
