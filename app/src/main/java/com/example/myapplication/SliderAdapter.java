4d9461dfbf6f5d7718407b9aed29fdf2bf999088
Sourav2507 <132735088+Sourav2507@users.noreply.github.com>
2024-07-26 21:17:40 +0530

    Onboarding activity added

--- app/src/main/java/com/example/myapplication/SliderAdapter.java
+++ app/src/main/java/com/example/myapplication/SliderAdapter.java
@@ -8,6 +8,7 @@ import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -57,9 +58,10 @@ public class SliderAdapter extends PagerAdapter {
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
 
         layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
-        View view = layoutInflater.inflate((R.layout.sliding_layout,container, attachToRoot: false);
+        View view = layoutInflater.inflate(R.layout.sliding_layout,container, false);
 
-        ImageWriter imageView = view.findViewById(R.id.slider_img);
+
+        ImageView imageView = view.findViewById(R.id.slider_img);
         TextView heading = view.findViewById(R.id.heading);
         TextView description = view.findViewById(R.id.description);
 
