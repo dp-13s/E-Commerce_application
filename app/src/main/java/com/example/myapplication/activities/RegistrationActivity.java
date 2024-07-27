0af8c51a666754a927286ca7580ec1a3ae13c590
Sourav2507 <132735088+Sourav2507@users.noreply.github.com>
2024-07-27 11:34:38 +0530

    Updated OnBoarding Screen

--- app/src/main/java/com/example/myapplication/activities/RegistrationActivity.java
+++ app/src/main/java/com/example/myapplication/activities/RegistrationActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -22,6 +23,8 @@ public class RegistrationActivity extends AppCompatActivity {
     EditText name,email,password;
     private FirebaseAuth auth;
 
+    SharedPreferences sharedPreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +41,22 @@ public class RegistrationActivity extends AppCompatActivity {
         name = findViewById(R.id.name);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
+
+        sharedPreferences = getSharedPreferences("onBoardingScreen",MODE_PRIVATE);
+
+        boolean isFirstTime = sharedPreferences.getBoolean("firstTime",true);
+
+        if(isFirstTime){
+
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean("firstTime",false);
+            editor.commit();
+
+            Intent intent = new Intent(RegistrationActivity.this,OnBoardingActivity.class);
+            startActivity(intent);
+            finish();
+
+        }
 
     }
     public  void signup(View view){
